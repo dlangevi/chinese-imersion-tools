f6a40c035c6fb5c29ed09a7373f476203f9c2db9
David Langevin <dlangevi@uwaterloo.ca>
2022-03-26 17:49:43 -0700

    remove old code from previous project that has been ported to new places

--- www/js/spinner.js
+++
@@ -1,9 +0,0 @@
-function showLoader() {
-  var loader = document.querySelector('#loader');
-  loader.style.display = "block"
-}
-
-function finishLoader() {
-  var loader = document.querySelector('#loader');
-  loader.style.display = "none"
-}
