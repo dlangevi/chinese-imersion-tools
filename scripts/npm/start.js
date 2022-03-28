b8dd6f45605c1723ea2f513ff7173335eb481d11
David Langevin <dlangevi@uwaterloo.ca>
2022-03-28 11:40:55 -0700

    update final paths to refelct script changes

--- scripts/npm/start.js
+++ scripts/npm/start.js
@@ -2,7 +2,7 @@ const concurrently = require('concurrently');
 const upath = require('upath');
 
 concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
+    { command: 'node scripts/npm/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     { command: 'nodemon src/app.js', name: 'RUN_NODE', prefixColor: 'bgBlue.bold' }
 ], {
     prefix: 'name',
