b217681d734c91897186e3ef07e9c58bce67f1fc
David Langevin <dlangevi@uwaterloo.ca>
2022-03-29 09:56:41 -0700

    _links holds my symlinks, its where I start vim. Also add back the demo charts for now

--- src/app.js
+++ src/app.js
@@ -74,6 +74,7 @@ app.post("/exportwords", (req, res, next) => {
     });
 });
 
+
 app.listen(3000, () => {
   console.log('Server started on port 3000')
 });
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+async function main() {
+  // TODO load some info from anki + from our set of words to display here
+
+}
+main()
